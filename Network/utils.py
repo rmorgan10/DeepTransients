7779d2b2824ab26d05b853172b23821b9113e2ea
Robert Morgan <robert.morgan@wisc.edu>
2021-03-19 14:49:10 -0500

    copy utils to Network dir

---
+++ Network/utils.py
@@ -0,0 +1,48 @@
+"""
+Helper functions
+"""
+
+import os
+import sys
+
+def get_dataset_name():
+    """
+    Collect dataset name from command line args
+
+    Returns:
+        dataset_name as string if command line arg is valid
+
+    Raises:
+        ValueError if passed dataset name is not valid
+        KeyError if no dataset name is passed
+    """
+
+    # Set allowed dataset names
+    datasets = ['full_data', 'high_cad_data', 'lsst_data', 'des_deep_data']
+
+    # Get dataset name
+    try:
+        dataset_name = sys.argv[1]
+    except IndexError:
+        raise KeyError("No dataset name was passed as a command line arg")
+
+    # Check that the name is valid
+    if dataset_name not in datasets:
+        raise ValueError(f"{dataset_name} is not a valid dataset")
+
+    return dataset_name
+
+
+def get_suffix():
+    # Get dataset name
+    try:
+        suffix = sys.argv[2]
+    except IndexError:
+        raise KeyError("No suffix name was passed as a command line arg")
+
+    # Check that suffix is valid
+    dataset_name = get_dataset_name()
+    if not os.path.exists(f'{dataset_name}/GROUP_1_ims_{suffix}.npy'):
+        raise ValueError(f"{suffix} is not a valid suffix for {dataset_name}")
+
+    return int(suffix)
